fcf1d9c1f01b8833ceb54cf6d9e611f2d29f9832
theDreamer911 <thetrulydreamer@gmail.com>
2021-01-25 22:37:15 +0700

    Make Array That Has Integer In it

--- arrayOfAlphabet/arrayOfAlphabet.js
+++ arrayOfAlphabet/arrayOfAlphabet.js
@@ -1,2 +1,2 @@
-alpha = Array.from(Array(26)).map((e, i) => i + 65);
-alphabet = alpha.map((x) => String.fromCharCode(x));
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const alphabet = alpha.map((x) => String.fromCharCode(x));
